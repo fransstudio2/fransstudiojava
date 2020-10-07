5d4b25e7ce7b759bc4eb2253cfd000a85438031a
Fransiskus Winata <fransiskus.winata@tui.com>
2020-10-07 23:23:08 +0200

    First commit

---
+++ src/main/java/com/fransstudiojava/fransstudiojava/FransstudiojavaApplication.java
@@ -0,0 +1,13 @@
+package com.fransstudiojava.fransstudiojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FransstudiojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FransstudiojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fransstudiojava/fransstudiojava/FransstudiojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fransstudiojava.fransstudiojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FransstudiojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
